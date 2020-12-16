4d763f564e418462fc257c0764fcc3e2d8443837
Suraj Mishra <isurajmisra@gmail.com>
2020-12-16 17:35:04 +0530

    added requirement and Procile

--- atlantic/settings.py
+++ atlantic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%59du%f=wudiafbs#tx5&-@izahwq^wfo+n!c+v7-#fz4tgy^@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['atlantic.herokuapp.com',]
+ALLOWED_HOSTS = ['atlantic-int.herokuapp.com',]
 
 
 # Application definition
