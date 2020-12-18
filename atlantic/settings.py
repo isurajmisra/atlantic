42b62b847b2f040192cf5f8898e1795cbe5fc245
Suraj Mishra <isurajmisra@gmail.com>
2020-12-18 12:59:41 +0530

    logout problem

--- atlantic/settings.py
+++ atlantic/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-LOGOUT_REDIRECT_URL = 'login'
+# LOGOUT_REDIRECT_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
